b879da1299497a6883452848410e7efbf77d52b5
Thomas Chen <qkthomas@gmail.com>
2017-10-29 13:29:37 -0400

    fix two triangle projects

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,17 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  args = [a, b, c].sort
+  raise(TriangleError, "Not a triangle!") if args[0] + args[1] <= args[2]
+  hash = Hash.new(0)
+  args.each do |key|
+    raise(TriangleError, "Not a triangle!") if key <= 0
+    hash[key] += 1
+  end
+  max = 0
+  hash.each {|key, value| max = value if value > max}
+  retvals = [:scalene, :isosceles, :equilateral]
+  return retvals[max-1]
 end
 
 # Error class used in part 2.  No need to change this code.
