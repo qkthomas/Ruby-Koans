0a7e5b7ab40abbe28b87e8c3115e61c0c645864d
Thomas Chen <qkthomas@gmail.com>
2017-10-29 00:44:53 -0400

    about_triangle_project.rb done

--- about_triangle_project.rb
+++ about_triangle_project.rb
@@ -3,6 +3,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # You need to write the triangle method in the file 'triangle.rb'
 require './triangle'
 
+def triangle (*args)
+  if args.size > 3
+	raise ArgumentError, "Not a triangle"
+  end
+  hash = Hash.new(0)
+  args.each {|key| hash[key] += 1}
+  max = 0
+  hash.each {|key, value| max = value if value > max}
+  retvals = [:scalene, :isosceles, :equilateral]
+  return retvals[max-1]
+end
+
 class AboutTriangleProject < Neo::Koan
   def test_equilateral_triangles_have_equal_sides
     assert_equal :equilateral, triangle(2, 2, 2)
